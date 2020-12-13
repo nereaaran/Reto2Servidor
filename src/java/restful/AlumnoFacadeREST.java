4596ee0c6c8becb53b360f0c68c3f85befb133f8
Kristina Milea <Kristina Milea@192.168.0.27>
2020-12-13 19:37:26 +0100

    Borrado un método.

--- src/java/restful/AlumnoFacadeREST.java
+++ src/java/restful/AlumnoFacadeREST.java
@@ -20,33 +20,19 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 /**
- * Clase que ejecuta las operaciones CRUD en la entidad Alumno.
- *
  * @author Cristina Milea
  */
 @Stateless
 @Path("entidad.alumno")
 public class AlumnoFacadeREST extends AbstractFacade<Alumno> {
-    
-    /**
-     * El Entity Manager se le pide al servidor de aplicaciones.
-     */
+
     @PersistenceContext(unitName = "Reto2ServidorPU")
     private EntityManager em;
 
-    /**
-     * Constructor que llama al constructor de la superclase.
-     */
     public AlumnoFacadeREST() {
         super(Alumno.class);
     }
 
-    /**
-     * Método que se va a ejecutar cuando le llegue una petición de tipo POST
-     * por HTTP.
-     *
-     * @param entity la entidad Alumno.
-     */
     @POST
     @Override
     @Consumes({MediaType.APPLICATION_XML})
@@ -54,12 +40,6 @@ public class AlumnoFacadeREST extends AbstractFacade<Alumno> {
         super.create(entity);
     }
 
-    /**
-     * Método que se va a ejecutar cuando le llegue una petición de tipo PUT por
-     * HTTP.
-     *
-     * @param entity la entidad Alumno.
-     */
     @PUT
     @Consumes({MediaType.APPLICATION_XML})
     @Override
@@ -67,25 +47,12 @@ public class AlumnoFacadeREST extends AbstractFacade<Alumno> {
         super.edit(entity);
     }
 
-    /**
-     * Método que se va a ejecutar cuando le llegue una petición de tipo DELETE
-     * por HTTP.
-     *
-     * @param id el id que se usará para buscar a un alumno.
-     */
     @DELETE
     @Path("{id}")
     public void remove(@PathParam("id") Integer id) {
         super.remove(super.find(id));
     }
 
-    /**
-     * Método que se va a ejecutar cuando le llegue una petición de tipo GET por
-     * HTTP.
-     *
-     * @param id el id que se usará para buscar a un alumno.
-     * @return el id que encuentre.
-     */
     @GET
     @Path("{id}")
     @Produces({MediaType.APPLICATION_XML})
@@ -93,11 +60,6 @@ public class AlumnoFacadeREST extends AbstractFacade<Alumno> {
         return super.find(id);
     }
 
-    /**
-     * Método que establece el Entity Manager.
-     *
-     * @return el Entity Manager
-     */
     @Override
     protected EntityManager getEntityManager() {
         return em;
